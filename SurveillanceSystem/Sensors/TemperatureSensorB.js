4af179c218af2b8723778a3dfdefd2d214d3e266
EmmaSalot <emma.slt59@gmail.com>
2026-03-23 09:00:36 +0100

    feat: add TemperatureSensorB with demo

--- SurveillanceSystem/Sensors/TemperatureSensorB.js
+++ SurveillanceSystem/Sensors/TemperatureSensorB.js
@@ -4,6 +4,11 @@ class ThermalSensorB {
     }  // position = id de la pièce
 
     triggerHeatSignature(process) {
-
+        const data = {
+            sensor: "ThermalSensor B : " + this.position,
+            detection: "thermal",
+            date: new Date().toISOString()
+        };
+        process(data);
     } // Déclenche une donnée complexe json, voir ci dessous
 }
--- SurveillanceSystem/index.js
+++ SurveillanceSystem/index.js
@@ -3,3 +3,6 @@ camera.onDetect((message) => console.log(message));
 
 const tempSensor = new TemperatureSensorA('Salle 2', 30);
 tempSensor.onDetect((message) => console.log(message));
+
+const thermalB = new ThermalSensorB('03');
+thermalB.triggerHeatSignature((data) => console.log(data));
